7cbeacda5e8c6e5c05a27fbf32628c354ca82a1c
Vastatio <solarcactus@live.com>
2015-08-01 11:57:47 -1000

    Added GuiBase: Immediately registers the GUI to the container with a super call. Changed GuiSmithy to extend GuiBase.

--- src/main/java/com/arkcraft/mod/core/machine/gui/GuiSmithy.java
+++ src/main/java/com/arkcraft/mod/core/machine/gui/GuiSmithy.java
@@ -1,18 +1,10 @@
 package com.arkcraft.mod.core.machine.gui;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-
-import org.lwjgl.opengl.GL11;
-
-import com.arkcraft.mod.core.Main;
 
 /***
  * 
@@ -20,31 +12,15 @@ import com.arkcraft.mod.core.Main;
  *
  */
 @SideOnly(Side.CLIENT)
-public class GuiSmithy extends GuiContainer {
-
-	private static final ResourceLocation tex = 
-			new ResourceLocation(Main.MODID, "textures/gui/smithy.png");
+public class GuiSmithy extends GuiBase {
 	
 	public GuiSmithy(InventoryPlayer invPlayer, World world, BlockPos pos) {
-		super(new ContainerSmithy(invPlayer, world, pos));
-		this.xSize = 182;
-		this.ySize = 226;
+		super(new ContainerSmithy(invPlayer, world, pos), "Smithy", "textures/gui/smithy.png", 182, 226);
 		
 	}
 	
 	public void onGuiClosed() { super.onGuiClosed(); }
-	
-	protected void drawGuiContainerForegroundLayer(int i, int j) {
-		this.fontRendererObj.drawString(StatCollector.translateToLocal("Smithy"), 96, 5, 0x000000);
-	}
-	
-	@Override
-	protected void drawGuiContainerBackgroundLayer(float partTick, int mX, int mY) {
-		/* Clear colors, bind texture, render */
-		GL11.glColor4f(1F, 1F, 1F, 1F);
-		if(tex == null) System.err.println("File is not found at path: " + tex.getResourcePath());
-		Minecraft.getMinecraft().getTextureManager().bindTexture(tex);
-		this.drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
-	}
+	protected void drawGuiContainerForegroundLayer(int i, int j) { super.drawGuiContainerForegroundLayer(i, j); }
+	protected void drawGuiContainerBackgroundLayer(float partTick, int mX, int mY) { super.drawGuiContainerBackgroundLayer(partTick, mX, mY); }
 
 }
