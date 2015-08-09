12137de67b0f05d39e82aab0c786a65eabb8840d
SolarCactus <solarcactus@live.com>
2015-08-09 11:04:21 -0700

    More stuff for event handler. One problem: The common proxy register was not working, so I removed it.

--- src/main/java/com/arkcraft/mod/core/handler/ARKEventHandler.java
+++ src/main/java/com/arkcraft/mod/core/handler/ARKEventHandler.java
@@ -1,7 +1,9 @@
 package com.arkcraft.mod.core.handler;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.EntityInteractEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -16,7 +18,8 @@ public class ARKEventHandler {
 	
 	@EventHandler
 	public void load(FMLInitializationEvent event) {
-		
+		MinecraftForge.EVENT_BUS.register(new ARKEventHandler());
+		FMLCommonHandler.instance().bus().register(MinecraftForge.EVENT_BUS);
 	}
 	
 	/**
@@ -43,11 +46,12 @@ public class ARKEventHandler {
 	 * 
 	 */
 	@SubscribeEvent
-	public void entityInteractEvent(EntityInteractEvent event) {
+	public void onEntityInteractEvent(EntityInteractEvent event) {
+		LogHelper.info("The player right clicked an entity at: " + event.entity.posX + ", " + event.entity.posY + "," + event.entity.posZ + ".");
 		if(event.target.interactFirst(event.entityPlayer)) return;
 		if(event.target instanceof EntityDodo && !event.entity.worldObj.isRemote && ((EntityDodo)event.entity).isChested() && !event.entityPlayer.isSneaking()) {
 			/* Opens the Smithy GUI for now, TODO get the right GUI opened. */
-			LogHelper.info("EntityInteractEvent: The player has interacted with EntityDodo at: " + event.entity.posX + event.entity.posY + event.entity.posZ);
+			LogHelper.info("EntityInteractEvent: The player has interacted with EntityDodo at: " + event.entity.posX + ", " + event.entity.posY + "," + event.entity.posZ + ".");
 			event.entityPlayer.openGui(Main.instance, GlobalAdditions.guiID++, event.entityPlayer.worldObj, event.entityPlayer.getPosition().getX(), event.entityPlayer.getPosition().getY(), event.entityPlayer.getPosition().getZ());
 		}
 	}
