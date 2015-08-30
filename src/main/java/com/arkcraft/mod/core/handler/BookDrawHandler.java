ba25cd2ca35b69f3d87cf774a484cc16eb2f6cfc
SolarCactus <solarcactus@live.com>
2015-08-29 21:34:47 -0700

    Added some stuff, deleted some stuff. drawTemperment was added to BookDrawHandler left and right were removed for drawTitle, i decided titles should be on right, and everything else about one mob should be on two pages. Added the enum CATEGORY. I am going to do something in which you can click on four squares for Dinos, and you go to the specific place in which those dinos are.

--- src/main/java/com/arkcraft/mod/core/handler/BookDrawHandler.java
+++ src/main/java/com/arkcraft/mod/core/handler/BookDrawHandler.java
@@ -14,45 +14,51 @@ import com.arkcraft.mod.core.entity.aggressive.EntityRaptor;
 
 public class BookDrawHandler {
 	
-	public enum DINO_NAMES {
-		ANKLOSAURUS("Ankylosaurus", "Ankylosaurus Crassacutis", "ark.book.ankylo.temperment", 0, 0),
-		BRONTOSAURUS("Brontosaurus", "Brontosaurus Lazarus", "ark.book.bronto.temperment", 0, 1),
-		CARNOTAURUS("Carnotaurus", "Carnotaurus Pressor", "ark.book.carno.temperment", 1, 2),
-		DILOPHOSAURUS("Dilophosaur", "Dilophosaurus Sputatrix", "ark.book.dilo.temperment", 1, 3),
-		UTAHRAPTOR("Raptor", "Utahraptor Prime", "ark.book.utahraptor.temperment", 1, 4),
-		TRICERATOPS("Triceratops", "Triceratops Styrax", "ark.book.triceratops.temperment", 0, 5),
-		TREX("Tyrannosaurus-Rex", "Tyrannosaurus Dominum", "ark.book.trex.temperment", 1, 6),
-		CARBONEMYS("Carbonemys", "Carbonemys Obibimus", "ark.book.carbonemys.temperment", 0, 7),
-		ICHTHYOSAURUS("Ichthyosaurus", "Ichthyosaurus Curiosa", "ark.book.ichthy.temperment", 1, 8);
+	public enum CATEGORY {
+		DINO_CARNIVORE,
+		DINO_OMNIVORE,
+		DINO_HERBIVORE,
+		DINO_OTHER;
+	}
+	
+	public enum DINO_NAME {
+		ANKLOSAURUS("Ankylosaurus", "Ankylosaurus Crassacutis", "ark.book.ankylo.description", 0, 0),
+		BRONTOSAURUS("Brontosaurus", "Brontosaurus Lazarus", "ark.book.bronto.description", 0, 1),
+		CARNOTAURUS("Carnotaurus", "Carnotaurus Pressor", "ark.book.carno.description", 1, 2),
+		DILOPHOSAURUS("Dilophosaur", "Dilophosaurus Sputatrix", "ark.book.dilo.description", 1, 3),
+		UTAHRAPTOR("Raptor", "Utahraptor Prime", "ark.book.utahraptor.description", 1, 4),
+		TRICERATOPS("Triceratops", "Triceratops Styrax", "ark.book.triceratops.description", 0, 5),
+		TREX("Tyrannosaurus-Rex", "Tyrannosaurus Dominum", "ark.book.trex.description", 1, 6),
+		CARBONEMYS("Carbonemys", "Carbonemys Obibimus", "ark.book.carbonemys.description", 0, 7),
+		ICHTHYOSAURUS("Ichthyosaurus", "Ichthyosaurus Curiosa", "ark.book.ichthy.description", 1, 8);
 		
-		String temperment;
+		String description;
 		String species;
 		String name;
 		int id;
 		int type;
 		
-		DINO_NAMES(String dinoName, String species, String temperment, int type, int id) {
+		DINO_NAME(String dinoName, String species, String description, int type, int id) {
 			this.name = dinoName;
 			this.id = id;
 			this.type = type;
 			this.species = species;
-			this.temperment = temperment;
+			this.description = description;
 		}
 		
 		public String getName() { return name; }
 		public int getID() { return id; }
-		public String getTemperment() { return StatCollector.translateToLocal(temperment); }
+		public String getDescription() { return StatCollector.translateToLocal(description); }
 		public String getSpecies() { return species; }
 		
 		/***
 		 * 
-		 * @return Passive or Aggresive
+		 * @return Docile or Aggresive
 		 */
 		public String getType() {
 			switch(type) {
-				case 0: return "Passive";
+				case 0: return "Docile";
 				case 1: return "Aggresive";
-				case 2: return "Passive";
 				default: return null;
 			}
 		}
@@ -76,41 +82,40 @@ public class BookDrawHandler {
 	
 	public static void drawPages(FontRenderer renderer, int mouseX, int mouseY, int currentPage) {
 		if(currentPage == 0) {
-			/* Page 1 */
-			drawTitle(renderer, true, DINO_NAMES.UTAHRAPTOR);
+			/* Page 1 / 2*/
+			drawTitle(renderer, DINO_NAME.UTAHRAPTOR);
 			GuiInventory.drawEntityOnScreen(getLeft() + 14, 90, 20, -350f, -5F, new EntityRaptor(Minecraft.getMinecraft().theWorld, 1));
-			drawDiet(renderer, getLeft() + 8, 140, DINO_NAMES.UTAHRAPTOR);
-			drawSpecies(renderer, getLeft() + 8, 150, DINO_NAMES.UTAHRAPTOR);
-			
-			/* Page 2 */
-			drawTitle(renderer, false, DINO_NAMES.TRICERATOPS);
-			
+			drawDiet(renderer, getLeft() + 40, 135, DINO_NAME.UTAHRAPTOR);
+			drawSpecies(renderer, getLeft() + 15, 150, DINO_NAME.UTAHRAPTOR);
 		}
 		else if(currentPage == 2) {
 			
 		}
 	}
 	
-	public static void drawSpecies(FontRenderer renderer, int x, int y, DINO_NAMES dinoName) {
+	public static void drawSpecies(FontRenderer renderer, int x, int y, DINO_NAME dinoName) {
 		GlStateManager.pushMatrix();
 		GL11.glScalef(0.7f, 0.7f, 1);
 		renderer.drawString("Species: " + dinoName.getSpecies(), x, y, Color.darkGray.getRGB());
 		GlStateManager.popMatrix();
 	}
 	
-	public static void drawDiet(FontRenderer renderer, int x, int y, DINO_NAMES dinoName) {
+	public static void drawTemperment(FontRenderer renderer, int x, int y, DINO_NAME dinoName) {
+		GlStateManager.pushMatrix();
+		GL11.glScalef(0.7f, 0.7f, 1);
+		renderer.drawString("Temperment: " + dinoName.getType(), x, y, Color.darkGray.getRGB());
+		GlStateManager.popMatrix();
+	}
+	
+	public static void drawDiet(FontRenderer renderer, int x, int y, DINO_NAME dinoName) {
         GlStateManager.pushMatrix(); /* Start the rendering */
 		GL11.glScalef(0.7f, 0.7f, 1); /* 70% the normal size */
 		renderer.drawString("Diet: " + dinoName.getPreference(), x, y, Color.darkGray.getRGB()); 
         GlStateManager.popMatrix(); /* End the rendering */
 	}
 	
-	
-	
-	public static void drawTitle(FontRenderer renderer, boolean left, DINO_NAMES dinoName) {
-		boolean right = !left;
-		if(left) renderer.drawString(dinoName.getName(), getLeft() - dinoName.getName().length(), 45, Color.darkGray.getRGB());
-		if(right) renderer.drawString(dinoName.getName(), getRight() - dinoName.getName().length(), 45, Color.darkGray.getRGB());
+	public static void drawTitle(FontRenderer renderer, DINO_NAME dinoName) {
+		/* Draws this on left */renderer.drawString(dinoName.getName(), getLeft() + 5 - dinoName.getName().length(), 45, Color.darkGray.getRGB());
 	}
 	
 	public static int getLeft() { return left; }
