341fa06f0f0f7716057ef4e13b8da10477c16fba
wildbill22 <billwerth2@msn.com>
2015-10-13 20:39:55 -0700

    Fix new ARKCraftItems instead of ModItems

--- src/main/java/com/arkcraft/mod/common/handlers/RecipeHandler.java
+++ src/main/java/com/arkcraft/mod/common/handlers/RecipeHandler.java
@@ -114,21 +114,21 @@ public class RecipeHandler {
 
 	public static void registerSmithyCraftingRecipes() {		
 		SmithyCraftingManager.getInstance().addShapelessRecipe(
-				new ItemStack(ModItems.saddle_small, 1), 
+				new ItemStack(ARKCraftItems.saddle_small, 1), 
 				new ItemStack(Items.leather, 15), 
 				new ItemStack(Items.iron_ingot, 10), 
-				new ItemStack(ModItems.fiber, 15)
+				new ItemStack(ARKCraftItems.fiber, 15)
 				);
-		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ModItems.saddle_medium, 1), 
+		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ARKCraftItems.saddle_medium, 1), 
 				new ItemStack(Items.leather, 48),
 				new ItemStack(Items.iron_ingot, 32), 
-				new ItemStack(ModItems.fiber, 32),
+				new ItemStack(ARKCraftItems.fiber, 32),
 				new ItemStack(Items.diamond, 1)
 				);
-		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ModItems.saddle_large, 1), 
-				new ItemStack(ModItems.chitin, 32),
+		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ARKCraftItems.saddle_large, 1), 
+				new ItemStack(ARKCraftItems.chitin, 32),
 				new ItemStack(Items.iron_ingot, 16), 
-				new ItemStack(ModItems.fiber, 32),
+				new ItemStack(ARKCraftItems.fiber, 32),
 				new ItemStack(Items.diamond, 4)
 				);
 //		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ModItems.boneHelm, 1),
@@ -147,20 +147,20 @@ public class RecipeHandler {
 //				new ItemStack(Items.bone, 16),
 //				new ItemStack(ModItems.bones_large, 7)
 //				);
-		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ModItems.longneck_rifle, 1),
+		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ARKCraftItems.longneck_rifle, 1),
 				new ItemStack(Items.iron_ingot, 32),
 				new ItemStack(Blocks.glass, 1)
 				);
-		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ModItems.shotgun, 1),
+		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ARKCraftItems.shotgun, 1),
 				new ItemStack(Items.iron_ingot, 32),
 				new ItemStack(Blocks.log, 4)
 				);
-		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ModItems.rocket_launcher, 1),
+		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ARKCraftItems.rocket_launcher, 1),
 				new ItemStack(Items.iron_ingot, 48),
 				new ItemStack(Items.diamond, 4),
 				new ItemStack(Items.redstone, 16)
 				);
-		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ModItems.explosive_ball, 1),
+		SmithyCraftingManager.getInstance().addShapelessRecipe(new ItemStack(ARKCraftItems.explosive_ball, 1),
 				new ItemStack(Items.iron_ingot, 3),
 				new ItemStack(Items.gunpowder, 4)
 				);
