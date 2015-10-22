717986f884f050332b222334fe630c0693792af1
BubbleTrouble14 <ronald.goedeke@outlook.com>
2015-10-22 22:26:18 +0200

    Added, dino and human gui, might still need a topor icon though

--- src/main/java/com/arkcraft/mod/common/items/weapons/component/RangedCompSpyGlass.java
+++ src/main/java/com/arkcraft/mod/common/items/weapons/component/RangedCompSpyGlass.java
@@ -1,8 +1,18 @@
 package com.arkcraft.mod.common.items.weapons.component;
 
+import java.util.List;
+
+import org.lwjgl.input.Keyboard;
+
+import com.arkcraft.mod.common.lib.KeyBindings;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class RangedCompSpyGlass extends RangedComponent
 {
@@ -13,12 +23,33 @@ public class RangedCompSpyGlass extends RangedComponent
 	}
 	
 	@Override
+    public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
+        player.setItemInUse(itemStack, this.getMaxItemUseDuration(itemStack));
+        return itemStack;
+    }
+    
+	@Override
+	public int getMaxItemUseDuration(ItemStack itemStack) 
+	{
+		return Integer.MAX_VALUE;
+	}
+	
+	@Override
 	public float getMaxZoom()
 	{
 		return 0.50f;
-	}
-
-
+	}	
+	
+	@SuppressWarnings("unchecked")
+	@Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean bool) {
+        list.add(StatCollector.translateToLocal("item.zoom.binoculars.desc.1"));
+        if (KeyBindings.playerScoping.getKeyCode() != 0) {
+            list.add(StatCollector.translateToLocalFormatted("item.zoom.binoculars.desc.2", EnumChatFormatting.AQUA + Keyboard.getKeyName(KeyBindings.playerScoping.getKeyCode()) + EnumChatFormatting.GRAY));
+        }
+    }	
+	
 
 	@Override
 	public void effectReloadDone(ItemStack itemstack, World world,
