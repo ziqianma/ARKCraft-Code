f736d0cbb87a5b10d344c02a9af103c05cfe1948
wildbill22 <billwerth2@msn.com>
2015-10-28 08:05:47 -0700

    fixed warning

--- src/main/java/com/arkcraft/mod/common/entity/EntityTameableDinosaur.java
+++ src/main/java/com/arkcraft/mod/common/entity/EntityTameableDinosaur.java
@@ -22,7 +22,6 @@ import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.potion.Potion;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
