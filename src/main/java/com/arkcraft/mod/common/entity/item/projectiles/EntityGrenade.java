d47e72a40d57f6db051e481983ecd46d5c5a5d05
BubbleTrouble14 <ronald.goedeke@outlook.com>
2015-11-07 18:01:19 +0100

    Oh, forgot to sync this to

--- src/main/java/com/arkcraft/mod/common/entity/item/projectiles/EntityGrenade.java
+++ src/main/java/com/arkcraft/mod/common/entity/item/projectiles/EntityGrenade.java
@@ -2,6 +2,7 @@ package com.arkcraft.mod.common.entity.item.projectiles;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
@@ -18,10 +19,32 @@ public class EntityGrenade extends EntityThrowable {
 
 	}
 
-	public EntityGrenade(World w, double par2, double par3, double par4) {
-		super(w, par2, par3, par4);
+	public EntityGrenade(World w, double x, double y, double z) {
+		super(w);
+		setPosition(x, y, z);
 
 	}
+	
+	public EntityGrenade(World world, EntityLivingBase entityliving, float f)
+	{
+		this(world);
+	//	getThrower = entityliving;
+		setLocationAndAngles(entityliving.posX, entityliving.posY + entityliving.getEyeHeight(), entityliving.posZ, entityliving.rotationYaw, entityliving.rotationPitch);
+		posX -= MathHelper.cos((rotationYaw / 180F) * 3.141593F) * 0.16F;
+		posY -= 0.1D;
+		posZ -= MathHelper.sin((rotationYaw / 180F) * 3.141593F) * 0.16F;
+		setPosition(posX, posY, posZ);
+		motionX = -MathHelper.sin((rotationYaw / 180F) * 3.141593F) * MathHelper.cos((rotationPitch / 180F) * 3.141593F);
+		motionY = -MathHelper.sin((rotationPitch / 180F) * 3.141593F);
+		motionZ = MathHelper.cos((rotationYaw / 180F) * 3.141593F) * MathHelper.cos((rotationPitch / 180F) * 3.141593F);
+		setSpeed(f);
+		
+	}
+	
+	private void setSpeed(float f)
+    {
+		setThrowableHeading(motionX, motionY, motionZ, f * 1.1F, 2.0F);
+    }
 
 	@Override
 	protected void onImpact(MovingObjectPosition par1) {
--- src/main/java/com/arkcraft/mod/common/items/ItemGrenade.java
+++ src/main/java/com/arkcraft/mod/common/items/ItemGrenade.java
@@ -6,7 +6,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;
 import net.minecraft.world.World;
 
 public class ItemGrenade extends Item{
@@ -16,28 +15,51 @@ public class ItemGrenade extends Item{
 	}
 	
 	@Override
-	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn) {
-    	if (!playerIn.capabilities.isCreativeMode) {
-            --itemStackIn.stackSize;
-        }
-    	worldIn.playSoundAtEntity(playerIn, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
-    	if (!worldIn.isRemote) {
-    		worldIn.spawnEntityInWorld(new EntityGrenade(worldIn, playerIn));
-    	}
-    	playerIn.triggerAchievement(StatList.objectUseStats[Item.getIdFromItem(this)]);
-    	return itemStackIn;
+	public void onPlayerStoppedUsing(ItemStack itemstack, World world, EntityPlayer entityplayer, int i)
+	{
+		if (!entityplayer.inventory.hasItem(this)) return;
+		
+		int j = getMaxItemUseDuration(itemstack) - i;
+		float f = j / 20F;
+		f = (f * f + f * 2.0F) / 3F;
+		if (f < 0.1F) return;
+		if (f > 1.0F)
+		{
+			f = 1.0F;
+		}
+
+		if (entityplayer.capabilities.isCreativeMode || entityplayer.inventory.consumeInventoryItem(this))
+		{
+			world.playSoundAtEntity(entityplayer, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 0.8F));
+			if (!world.isRemote)
+			{
+				EntityGrenade entiyGrenade = new EntityGrenade(world, entityplayer);
+				world.spawnEntityInWorld(entiyGrenade);
+			}
+		}
 	}
 	
 	@Override
 	public EnumAction getItemUseAction(ItemStack itemstack)
 	{
-		return EnumAction.BOW;
+		return EnumAction.BLOCK;
 	}
 	
-	 public int getMaxItemUseDuration(ItemStack stack)
-	 {
-	        return 72000;
-	 }
+	@Override
+	public int getMaxItemUseDuration(ItemStack stack)
+	{
+		return 0x11940;
+	}
+	 
+	@Override
+	public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer)
+		{
+			if (entityplayer.inventory.hasItem(this))
+			{
+				entityplayer.setItemInUse(itemstack, getMaxItemUseDuration(itemstack));
+			}
+			return itemstack;
+		} 
 	
 }
 
