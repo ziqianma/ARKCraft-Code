e671c3dc7c9afbbbeba01472e14527ccad10a6a6
Wim Creuwels <wim.creuwels@hotmail.com>
2016-02-04 00:13:58 +0100

    Raptor ColourTypes

--- src/main/java/com/arkcraft/module/creature/client/render/RenderRaptor.java
+++ src/main/java/com/arkcraft/module/creature/client/render/RenderRaptor.java
@@ -18,26 +18,37 @@ import com.arkcraft.module.creature.common.entity.test.EntityRaptor;
 public class RenderRaptor extends RenderLiving
 {
 	private static ResourceLocation[] texture;
-	protected ModelRaptorNew modelEntity;
 	private static final ResourceLocation raptorTexture = new ResourceLocation(
 			ARKCraft.MODID + ":textures/model/raptor.png");
+	protected ModelRaptorNew modelEntity;
 
 	public RenderRaptor(ModelBase base, float par2)
 	{
 		super(Minecraft.getMinecraft().getRenderManager(), base, par2);
 		modelEntity = ((ModelRaptorNew) mainModel);
 		texture = new ResourceLocation[RaptorType.numRaptors];
-		texture[0] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_albino.png");
-		texture[1] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_breen_white.png");
-		texture[2] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_cyan_lgreen.png");
-		texture[3] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_gay_gay.png");
-		texture[4] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_green_grey.png");
-		texture[5] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_green_tan.png");
-		texture[6] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_green_white.png");
-		texture[7] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_grey_grey.png");
-		texture[8] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_lbrown_tan.png");
-		texture[9] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_red_tan.png");
-		texture[10] = new ResourceLocation(ARKCraft.MODID + ":textures/model/raptor_tan_white.png");
+		texture[0] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_albino.png");
+		texture[1] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_breen_white.png");
+		texture[2] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_cyan_lgreen.png");
+		texture[3] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_gay_gay.png");
+		texture[4] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_green_grey.png");
+		texture[5] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_green_tan.png");
+		texture[6] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_green_white.png");
+		texture[7] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_grey_grey.png");
+		texture[8] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_lbrown_tan.png");
+		texture[9] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_red_tan.png");
+		texture[10] = new ResourceLocation(
+				ARKCraft.MODID + ":textures/model/raptor_tan_white.png");
 	}
 
 	public void render(EntityRaptor entity, double x, double y, double z, float u, float v)
@@ -59,11 +70,6 @@ public class RenderRaptor extends RenderLiving
 	protected ResourceLocation getEntityTexture(Entity e)
 	{
 		return raptorTexture;
-		/*
-		 * EntityRaptor r = (EntityRaptor) e; int type = r.getRaptorType(); //
-		 * return new ResourceLocation(ARKCraft.MODID + //
-		 * ":textures/model/raptor.png"); // return (texture[type]);//
-		 * texture[((EntityRaptor)e).raptorType];
-		 */
+		// return texture[((EntityRaptor) e).getType().getRaptorTypeInt()];
 	}
 }
